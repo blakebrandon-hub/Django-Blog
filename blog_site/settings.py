0758ed8a2ee44498daf7e1f03547ff9b0b5ba657
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2021-02-27 07:40:48 -0600

    Update settings.py

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 LOGOUT_REDIRECT_URL = 'home'
 
 LOGIN_REDIRECT_URL = 'home'
+
+django_heroku.settings(locals())

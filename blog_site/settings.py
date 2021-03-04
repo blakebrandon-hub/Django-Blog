734c371a49c59a5c0c879a5edb02fbcd9ce9d953
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2021-03-04 07:31:32 -0600

    Update settings.py

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -124,19 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-"""
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)"""
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-LOGOUT_REDIRECT_URL = 'home'
-
-LOGIN_REDIRECT_URL = 'home'
-
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
